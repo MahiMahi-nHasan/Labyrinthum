440a049636b082fc5ef541c5bc05b756d0422ea0
judeaustin <judeaustin122008@gmail.com>
2026-01-06 11:59:51 -0500

    resetting SampleScene

--- Assets/Scripts/LootTable.cs
+++ Assets/Scripts/LootTable.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class LootTable 
+public static class LootTable
 {
     // Start is called before the first frame update
-    static List<int> loot = new() {1,2,3,4,5,6,7,8}; 
+    static List<int> loot = new() { 1, 2, 3, 4, 5, 6, 7, 8 };
     // In order: Demon Armor, Sage Armor, Nightmare Mace, 
     public static Stack<int> s = new Stack<int>();
-    
+
     public static void Initialize()
     {
         int size = loot.Count;
 
-        while (size>0)
+        while (size > 0)
         {
-            int rand = Random.Range(0,size);
+            int rand = Random.Range(0, size);
             s.Push(loot[rand]);
             //Debug.Log("Pushed {s}!"+loot[rand]);
             loot.Remove(loot[rand]);
@@ -23,12 +23,12 @@ public static class LootTable
 
 
         }
-        
+
     }
     // Update is called once per frame
     public static int GetItem()
     {
         return s.Pop();
     }
-    
+
 }
