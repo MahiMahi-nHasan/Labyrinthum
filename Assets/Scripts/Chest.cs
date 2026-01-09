81e305d9457e95aa6d436734d2e5da4926e14ecb
judeaustin <judeaustin122008@gmail.com>
2026-01-09 12:10:28 -0500

    Finished chests

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -5,6 +5,16 @@ public class Chest : Interactable
     public Equipment[] lootIndex;
     private bool opened = false;
 
+    // For use with Billboard shader graph
+    public SpriteRenderer rendererObject;
+    public Sprite closedSprite;
+    public Sprite openSprite;
+
+    void Start()
+    {
+        rendererObject.sprite = closedSprite;
+    }
+
     public override void Interact()
     {
         if (!opened)
@@ -12,6 +22,8 @@ public class Chest : Interactable
             int id = LootTable.GetItem();
 
             InventoryManager.inventory.Add(lootIndex[id]);
+
+            rendererObject.sprite = openSprite;
         }
 
         opened = true;
